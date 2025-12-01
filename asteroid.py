adabd1781b100ee68bbf39927eb208280ca9e868
KielerSeaGull <joel@blaich.rocks>
2025-12-01 20:10:04 +0100

    Astroids Grundspiel fertig - LvL wird in Asteroiden gezeichnet Ideen: Klasse: Hub: an Bildschirmleiste (rechts?) Score & Leben anzeigen lassen. 3 Leben + Scoresystem einführen + Beschleunigung und Bremsen implementieren + Verschiedene Waffenpickups (überspringe 2 lvl o,ä.) einbauen.

--- asteroid.py
+++ asteroid.py
@@ -10,33 +10,35 @@ class Asteroid(CircleShape):
     
     def draw(self, screen):
         pygame.draw.circle(screen, "White", self.position, self.radius, LINE_WIDTH)
-
-        pygame.draw.lines(screen, "white", False, XXXX, LINE_WIDTH)
+        self.draw_lvl(screen)
     
     def update(self, dt: float):
         self.position += self.velocity * dt 
     
-    def get_pos_lvl_draw(self) -> list[tuple[int,int]]:
-        positions: list[tuple] = []
+    def draw_lvl(self, screen) -> None:
         center_x = self.position[0]
         center_y = self.position[1]
 
         # gerade Zahlen
         if self.get_lvl() % 2 == 0:
             for i in range(1,self.get_lvl()+1):
+                positions: list[tuple] = []
                 #(-1)**i ==> alterniert zwischen - (links) und + (rechts) vom center
                 # center + halber Abstand dann in Abhängigkeit von i (lvl) wie oft der Abstand kommen muss
-                my_pos_x = center_x + ((RENDER_SPACE_BETWEEN_LINES//2) + (i-1//2) * RENDER_SPACE_BETWEEN_LINES) * (-1)**i
+                my_pos_x = center_x + ((RENDER_SPACE_BETWEEN_LINES//2) + ((i-1)//2) * RENDER_SPACE_BETWEEN_LINES) * (-1)**i
 
                 # halbe linie nach unten und oben von centerhöhe zeichnen
                 # j in range(i,i+2) macht, dass wir immer auf der gleichen Höhe wie zuvor sind
                 for j in range(i,i+2):
                     my_pos_y = center_y + (RENDER_LINE_HIGHT//2) * (-1)**j
                     positions.append((my_pos_x, my_pos_y))
+                pygame.draw.line(screen, "White", positions[0], positions[1],LINE_WIDTH)
+
 
         # ungerade Zahlen
         else:
             for i in range(1,self.get_lvl()+1):
+                positions: list[tuple] = []
                 #(-1)**i ==> alterniert zwischen - (links) und + (rechts) vom center
                 # center + in Abhängigkeit von i (lvl) wie oft der Abstand kommen muss
                 my_pos_x = center_x + ((i//2) * RENDER_SPACE_BETWEEN_LINES) * (-1)**i
@@ -44,13 +46,10 @@ class Asteroid(CircleShape):
                 # halbe linie nach unten und oben von centerhöhe zeichnen
                 # j in range(i,i+2) macht, dass wir immer auf der gleichen Höhe wie zuvor sind
                 for j in range(i,i+2):
-                    my_pos_y = center_y + (RENDER_LINE_HIGHT//2) * (-1)**j
+                    my_pos_y = center_y + ((self.get_lvl() * RENDER_LINE_HIGHT)//2) * (-1)**j
                     positions.append((my_pos_x, my_pos_y))
+                pygame.draw.line(screen, "White", positions[0], positions[1],LINE_WIDTH)
 
-
-            #do something
-
-        return positions
 
 
     def split(self):
